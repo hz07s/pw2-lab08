d99b3e2f663232405989a305586db8eac2e382e0
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-15 17:04:28 -0500

    Configurando en settings.py del proyecto email_example para enviar correos electronicos utilizando el servidor SMTP de gmail

--- email_example/email_example/settings.py
+++ email_example/email_example/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =  587
+EMAIL_HOST_USER = 'hchoquehuancaz@unsa.edu.pe'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
